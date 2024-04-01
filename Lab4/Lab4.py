6ea75a120d2c2e6272de1692dea57a63d83cb8a7
LiviuTofan <liviutofan@mail.ru>
2024-04-02 01:41:44 +0300

    Corrected code, without hardcode, now it is autmatically

--- Lab4/Lab4.py
+++ Lab4/Lab4.py
@@ -29,7 +29,7 @@ def star(symbol: str):
 
 def power(symbol: str, n: int):
     repeated_symbol = ""
-    for i in range(0, n):
+    for i in range(0, n-1):
         repeated_symbol += symbol
     print(f'Repeat {symbol} {n} times')
     return repeated_symbol
@@ -39,50 +39,72 @@ def const(symbol: str):
     print(f'Constant {symbol}')
     return symbol
 
+def find_chooses(regex):
+    stack = []
+    for i, char in enumerate(regex):
+        if char == "(":
+            stack.append(i)
+        elif char == ")":
+            if stack:
+                matches = []
+                start_index = stack.pop()
+                substring = regex[start_index + 1:i]
+                index = len(substring) + 2
+                substring = substring.replace("|", "")
+                matches.extend(substring)
+                return matches, index
 
-def first_regex():
-    "(S|T)(U|V)W*Y+24"
+def find_operators(regex):
+    num = ""
     result = ""
-    result += choose(["S", "T"])
-    result += choose(["U", "V"])
-    result += star("W")
-    result += plus("Y")
-    result += "24"
+    i = 0
+    while i < len(regex):
+        char = regex[i]
+        # Find chooses
+        if char == "(":
+            result += const(num)
+            matches, index = find_chooses(regex[i:])
+            result += choose(matches)
+            i += index  # Increment index to skip over processed characters
+            num = ""
+            continue
+        # Check for constants
+        elif char.isalnum():
+            num += char
+        # Perform * operation
+        elif char == "*":
+            if num != "":
+                result += star(num)
+            else:
+                result += star(result[-1])
+            num = ""
+        # Perform + operation
+        elif char == "+":
+            if num != "":
+                result += plus(num)
+            else:
+                result += plus(result[-1])
+            num = ""
+        # Perform power operation
+        elif char == "^":
+            if num != "":
+                result += power(num, int(regex[i+1]))
+            else:
+                result += power(result[-1], int(regex[i + 1]))
+            i += 1
+            num = ""
+
+        i += 1  # Increment index to move to the next character
+    # Perform remaining operations if num is not empty
+    if num:
+        result += const(num)
+
     return result
 
-
-def second_regex():
-    "L(M|N)O^3P*Q(2|3)"
-    result = ""
-    result += const("L")
-    result += choose(["M", "N"])
-    result += power("O", 3)
-    result += star("P")
-    result += const("Q")
-    result += choose(["2", "3"])
-    return result
-
-
-def third_regex():
-    "R*S(T|U|V)W(X|Y|X)^2"
-    result = ""
-    result += star("R")
-    result += const("S")
-    result += star(choose(["T", "U", "V"]))
-    result += const("W")
-    result += power(choose(["X", "Y", "Z"]), 2)
-    return result
-
-
-print("------------")
-print("First regex")
-print("------------")
-print("Generated String:", first_regex())
-print("------------")
-print("Second regex")
-print("------------")
-print("Generated String:", second_regex())
-print("------------")
-print("Third regex")
-print("------------")
-print("Generated String:", third_regex())
+regexes = ["(S|T)(U|V)W*Y+24", "L(M|N)O^3P*Q(2|3)", "L(M|N)O^3P*Q(2|3)"]
+for regex in regexes:
+    print("------------")
+    print("Regex: ", regex)
+    print("------------")
+    result = find_operators(regex)
+    print(result)
